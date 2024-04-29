a16e595fed2f5a373bbcfcc433fc9c923402806a
muasle <sambagellis@gmail.com>
2024-04-29 11:13:11 -0700

    playtest push
    added gunshot audio and more bunnies for playtesting

--- Assets/Scripts/PewPew.cs
+++ Assets/Scripts/PewPew.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PewPew : MonoBehaviour
 {
     public Transform bulletSpawn;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
+    public AudioClip shootingSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
+            // Play shooting sound
+            if (shootingSound != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(shootingSound);
+            }
+
             var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
             bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.forward * bulletSpeed;
         }
