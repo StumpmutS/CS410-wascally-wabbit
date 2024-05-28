fc2a6d8def347a71525d469bfc199cfdacb09f5a
cho205224 <cho205224@gmail.com>
2024-05-28 15:41:03 -0700

    prey/hunter pos, hunter logic
    Copy hunter/prey and placed around map, made hunter able to be shot

--- Assets/Game/Movement/MovementAgent.cs
+++ Assets/Game/Movement/MovementAgent.cs
@@ -9,6 +9,10 @@ public class MovementAgent : MonoBehaviour
     [SerializeField] private NavMeshAgent agent;
 
     private bool _pathSet;
+    private bool hasBeenShot = false;
+    private bool canBeShot = true;
+    public float timePause = 5;
+    public float pauseReset = 5;
     
     public UnityEvent<MovementAgent> OnDestinationReached = new();
     
@@ -24,6 +28,27 @@ public class MovementAgent : MonoBehaviour
 
     private void Update()
     {
+        if (hasBeenShot)
+        {
+            timePause -= Time.deltaTime;
+            if (timePause <= 0)
+            {
+                timePause = 5;
+                agent.speed = 5;
+                hasBeenShot = false;
+                canBeShot = false;
+            }
+        }
+        if (canBeShot == false)
+        {
+            pauseReset -= Time.deltaTime;
+            if (pauseReset <= 0)
+            {
+                pauseReset = 5;
+                canBeShot = true;
+            }
+        }
+        
         if (!agent.enabled) return;
         if (agent.pathPending) return;
         if (agent.hasPath) _pathSet = true;
@@ -37,5 +62,17 @@ public class MovementAgent : MonoBehaviour
         agent.ResetPath();
         _pathSet = false;
         OnDestinationReached.Invoke(this);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            if (canBeShot == true)// && pauseReset > 0)
+            {
+                agent.speed = 0;
+                hasBeenShot = true;
+            }
+        }
     }
 }
